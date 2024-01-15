f5ca928cb29fe2af9b776d6351718e6a1830e45f
Jagadeeshwar Dabbadi <jag@intercotradingco.com>
2024-01-15 14:36:29 -0600

    Header completed

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./Components/Header"
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <section id="home">
+      {/* Content for Home section */}
+    </section>
+    <section id="about">
+      {/* Content for About section */}
+    </section>
+    <section id="projects">
+      {/* Content for Projects section */}
+    </section>
+    <section id="resume">
+      {/* Content for Resume section */}
+    </section>
+    <section id="contact">
+      {/* Content for Contact section */}
+    </section>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import download from '../Assests/download.jpg'
+import Button from '@mui/material/Button';
+
+function Header() {
+  // This function will scroll to the desired section
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
+  return (
+    <AppBar position="fixed" style={{ background: '#000' }}>
+    <Toolbar>
+        
+        <div style={{ flexGrow: 1 }}>
+          <img src={download} alt="Logo" style={{ maxHeight: '50px', margin: '0 30px' }} />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+
+
+        <Button color="inherit" onClick={() => scrollToSection('home')} style={{ margin: '0 30px' }}>Home</Button>
+        <Button color="inherit" onClick={() => scrollToSection('about')} style={{ margin: '0 30px' }}>About</Button>
+        <Button color="inherit" onClick={() => scrollToSection('projects')}style={{ margin: '0 30px' }}>Projects</Button>
+        <Button color="inherit" onClick={() => scrollToSection('resume')}style={{ margin: '0 30px' }}>Resume</Button>
+        <Button color="inherit" onClick={() => scrollToSection('contact')}style={{ margin: '0 30px' }}>Contact</Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
