6e7e6f094df8f63d146061051ba34923a1a23436
Jagadeeshwar011 <Jagadeeshwarreddy011@gmail.com>
2024-01-22 13:47:29 -0600

    Updated Skills Section

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,40 +13,45 @@ import AWS from "../Assests/aws.png"
 import Mongo from "../Assests/Mongo.png"
 import Git from "../Assests/Github.png"
 import Figma from "../Assests/Figma.png"
+import Skillsimage from "../Assests/Skills image.png"
 
 
 
 const skills = {
-  Languages: [
-    {  icon: HTML5Icon },
-    { icon: CSS3Icon },
-    { icon: JavaScriptIcon },
-    {  icon: NodejsIcon },
-   
-  ],
+    Languages: [
+        {icon: HTML5Icon},
+        {icon: CSS3Icon},
+        {icon: JavaScriptIcon},
+        {icon: NodejsIcon},
+
+    ],
   "Library & Frameworks": [
     {  icon: react },
     {  icon: Tailwind },
     {  icon: Mui },
     {  icon: Ex },
-    
+
   ],
   "Tools & Technologies": [
     {  icon: AWS },
     {  icon: Mongo },
     {  icon: Git },
     {  icon: Figma },
-    
+
   ],
 };
 const SkillIcon = ({ icon, name }) => (
-  <Box sx={{ textAlign: 'center', m: 2 }}>
-    <Avatar src={icon} sx={{ width: 56, height: 56, mx: 'auto', mb: 1 }} />
-    <Typography variant="caption" sx={{ mt: 1, color: 'white' }}>
-      {name}
-    </Typography>
-  </Box>
+    <Box sx={{ textAlign: 'center', margin: 2 }}>
+        <Avatar src={icon} sx={{ width: 56, height: 56, mx: 'auto', mb: 1 }} />
+        <Typography variant="caption" sx={{ mt: 1, color: 'white' }}>
+            {name}
+        </Typography>
+    </Box>
 );
+
+
+
+
 
 const About = () => {
 // Add this useEffect to ensure that the body does not overflow horizontally
@@ -69,25 +74,28 @@ useEffect(() => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'flex-start',
-        width :'100%',
         maxWidth: '100vw', // This ensures that the Box does not exceed the viewport width
-        overflowX: 'hidden', 
+        overflowX: 'hidden',
+        width: '100vw',
+        mt: 0,
+
       }}
     >
 
-      <Grid container spacing={0} alignItems="center" justifyContent="center" sx={{ maxWidth: '100%', m: 0, p: 0 }}>
-        {/* Profile Picture */}
+      <Grid container spacing={2} alignItems="center" justifyContent="center" sx={{ maxWidth: '100%', m: 0, p: 0 }}>
+
+      {/* Profile Picture */}
         <Grid item xs={12} md={4} lg={3}>
           <Avatar
             alt="Jagadeeshwar Dabbadi"
             src={profilePic}
             sx={{ width: 250, height: 250, mb: 4, mt: 4, mx: 'auto' }}
           />
-          
+
         </Grid>
         {/* Text Content */}
         <Grid item xs={12} md={8} lg={9} sx={{ p: 4, backgroundColor: 'rgba(255, 255, 255, 0.1)', width: '100%' }}>
-        
+
           <Typography variant="h4" component="h4" gutterBottom sx={{ fontWeight: 500 }}>
             Jagadeeshwar Dabbadi
           </Typography>
@@ -118,36 +126,62 @@ useEffect(() => {
             MY RESUME
           </Button>
         </Grid>
-        <Box sx={{ width: '100%', mt: 4, pl: 15 }}> {/* Add padding left to align with the content above */}
-        <Typography variant="h4" gutterBottom sx={{ color: '#fff' }}>
-          My Skills
-        </Typography>
-        <Grid container spacing={2} alignItems="center" justifyContent="flex-start"> {/* Justify to the start */}
-          {Object.entries(skills).map(([category, skillsArray]) => (
-            <Grid item xs={12} key={category}>
-              <Typography variant="h6" gutterBottom sx={{ color: '#FFD700' }}>
-                {category}:
-              </Typography>
-              <Grid container justifyContent="flex-start" spacing={2}> {/* Justify to the start */}
-                {skillsArray.map((skill, index) => (
-                  <Grid item key={index}>
-                    <SkillIcon icon={skill.icon} name={skill.name} />
-                  </Grid>
-                ))}
-                
-              </Grid>
-            </Grid>
-          ))}
-        </Grid>
-        
-      
-      </Box>
+        <Grid container item spacing={2} justifyContent="flex-end" alignItems="flex-start">
 
-      
+        {/* Skills List */}
+          <Grid item xs={12} md={4}>
+            <Typography variant="h4" gutterBottom sx={{ color: '#fff', mt: 4 }}>
+              My Skills
+            </Typography>
+            {Object.entries(skills).map(([category, skillsArray]) => (
+                <Box key={category}>
+                  <Typography variant="h6" gutterBottom sx={{ color: '#FFD700' }}>
+                    {category}:
+                  </Typography>
+                    <Grid container spacing={{ xs: 2, sm: 3, md: 4 }} justifyContent="flex-start" alignItems="center">
+                        {skillsArray.map((skill, index) => (
+                            <Grid item xs={6} sm={4} md={2} lg={2} key={index}>
+                                <SkillIcon icon={skill.icon} name={skill.name} />
+                            </Grid>
+                        ))}
+                    </Grid>
+
+
+                </Box>
+            ))}
+          </Grid>
+
+          {/* Image next to Skills */}
+          <Grid item xs={12} md={7}>
+              <Box
+                  sx={{
+                      backgroundColor: 'black', // Sets the background color of the image container to black
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      height: '100%', // Adjust the height as needed
+                      width: '100%',
+
+                  }}
+              >
+                  <img
+                      src={Skillsimage}
+                      alt="Skill Image"
+                      style={{
+                          maxHeight: '50%', // Adjust the maximum height as needed
+                          maxWidth: '50%', // Adjust the maximum width as needed
+                          opacity: 0.7// Set the opacity of the image itself to 1 (fully visible)
+                      }}
+                  />
+              </Box>
+          </Grid>
+        </Grid>
+
+
         </Grid>
         </Box>
-    
-    
+
+
   );
 };
 export default About;
